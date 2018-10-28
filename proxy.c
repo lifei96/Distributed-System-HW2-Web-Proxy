b4a31042bac3f8dd28dc2f86999b10bee0315368
Fei Li <lifei9696@gmail.com>
2018-10-27 21:12:43 -0400

    add SIGIGN

--- proxy.c
+++ proxy.c
@@ -23,6 +23,7 @@ void client_error(int fd, char *cause, char *errnum,
         char *shortmsg, char *longmsg);
 
 int main(int argc, char **argv) {
+    Signal(SIGPIPE, SIG_IGN);
     int listenfd, fd_client;
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
